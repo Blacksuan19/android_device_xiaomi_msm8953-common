8f74f4c9a8e77683cd819cec457761c261482aa1
blacksuan19 <i@blacksuan19.me>
2020-04-15 17:02:50 +0800

    msm8953-common: fix data/calling sim getting reset after reboot
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

--- prop.mk
+++ prop.mk
@@ -220,7 +220,8 @@ persist.vendor.vt.supported=1 \
 persist.vendor.data.iwlan.enable=true \
 persist.vendor.sys.cnd.iwlan=1 \
 persist.vendor.cne.logging.qxdm=3974 \
-persist.sys.fflag.override.settings_network_and_internet_v2=true
+persist.sys.fflag.override.settings_network_and_internet_v2=true \
+persist.vendor.radio.aosp_usr_pref_sel=true
 
 # RCS
 PRODUCT_PROPERTY_OVERRIDES += \
